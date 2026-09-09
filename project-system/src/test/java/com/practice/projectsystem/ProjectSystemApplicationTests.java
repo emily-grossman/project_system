dc2c72708532f5c3a4ae8bb64be5c8a35b5a3b91
emily-grossman <vkustova02@gmail.com>
2026-09-09 14:57:10 +0300

    init commit

---
+++ project-system/src/test/java/com/practice/projectsystem/ProjectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.projectsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
